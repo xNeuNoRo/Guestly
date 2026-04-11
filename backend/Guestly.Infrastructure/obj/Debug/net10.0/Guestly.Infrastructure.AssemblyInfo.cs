85f37b037bdcb844d80e42a5c00c93de2e09f928
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-04-10 22:19:46 -0400

    Update assembly information and add dotnet-tools configuration

--- backend/Guestly.Infrastructure/obj/Debug/net10.0/Guestly.Infrastructure.AssemblyInfo.cs
+++ backend/Guestly.Infrastructure/obj/Debug/net10.0/Guestly.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Guestly.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8833abceec7305a1764bcfce70ef596bf5ecaf55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Guestly.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Guestly.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
