1b4dac833029da0f85977afe5047bbd203e618b3
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-04-11 15:53:29 -0400

    Add DTOs for authentication: AuthResponse, LoginRequest, and RegisterRequest

---
+++ backend/Guestly.Application/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,34 @@
+namespace Guestly.Application.DTOs.Auth;
+
+public record AuthResponse()
+{
+    /// <summary>
+    /// El identificador único del usuario autenticado.
+    /// </summary>
+    public required Guid Id { get; init; }
+
+    /// <summary>
+    /// El nombre del usuario autenticado.
+    /// </summary>
+    public required string FirstName { get; init; }
+
+    /// <summary>
+    /// El apellido del usuario autenticado.
+    /// </summary>
+    public required string LastName { get; init; }
+
+    /// <summary>
+    /// La dirección de correo electrónico del usuario autenticado.
+    /// </summary>
+    public required string Email { get; init; }
+
+    /// <summary>
+    /// El rol del usuario autenticado.
+    /// </summary>
+    public required int Role { get; init; }
+
+    /// <summary>
+    /// El token de acceso JWT (Access Token) utilizado para autorizar las peticiones del usuario autenticado.
+    /// </summary>
+    public required string Token { get; init; }
+}
