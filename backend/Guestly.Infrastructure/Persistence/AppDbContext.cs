913891b6cb812b0ba646b94219b1a4f40fa82369
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-04-18 18:54:56 -0400

    Enhance AppDbContext to track creation and update timestamps using IDateTimeProvider

--- backend/Guestly.Infrastructure/Persistence/AppDbContext.cs
+++ backend/Guestly.Infrastructure/Persistence/AppDbContext.cs
@@ -1,16 +1,23 @@
+using Guestly.Domain.Entities.Base;
 using Guestly.Domain.Entities.Notifications;
 using Guestly.Domain.Entities.Properties;
 using Guestly.Domain.Entities.Reservations;
 using Guestly.Domain.Entities.Reviews;
 using Guestly.Domain.Entities.User;
+using Guestly.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Guestly.Infrastructure.Persistence;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options) { }
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options, IDateTimeProvider dateTimeProvider)
+        : base(options)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
 
     // Tablas principales que tendremos en la BD
     public DbSet<User> Users { get; set; }
@@ -20,6 +27,31 @@ public class AppDbContext : DbContext
     public DbSet<Reservation> Reservations { get; set; }
     public DbSet<Review> Reviews { get; set; }
     public DbSet<Notification> Notifications { get; set; }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        // Filtramos todas las entidades que hereden de BaseEntity y hayan sido modificadas o agregadas
+        var entries = ChangeTracker.Entries<BaseEntity>();
+
+        // Por cada entrada del ChangeTracker de EF Core
+        foreach (var entry in entries)
+        {
+            switch (entry.State)
+            {
+                // Si la entidad es nueva (Added), asignamos la fecha de creación (CreatedAt) con la fecha y hora actual en UTC
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = _dateTimeProvider.UtcNow;
+                    break;
+                // Si la entidad ha sido modificada (Modified), 
+                // asignamos la fecha de actualización (UpdatedAt) con la fecha y hora actual en UTC
+                case EntityState.Modified:
+                    entry.Entity.UpdatedAt = _dateTimeProvider.UtcNow;
+                    break;
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
